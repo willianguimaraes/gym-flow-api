0bc5b1c6291d07c729a38cf2cbfa7875dd2ed6e2
Willian Fernandes Guimaraes <e_willian.guimaraes@metrosp.com.br>
2026-06-11 01:03:32 -0300

    Ajuste erro de compilação.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,28 @@
-import { PrismaClient } from '../generated/prisma';
-import { PrismaNeon } from '@prisma/adapter-neon';
 import 'dotenv/config';
 
-const DATABASE_URL = process.env.DATABASE_URL!;
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaClient } from '@prisma/client';
+
+const DATABASE_URL = process.env.DATABASE_URL;
 
 if (!DATABASE_URL) {
-  throw new Error('DATABASE_URL não definida nas variáveis de ambiente.');
+  throw new Error('DATABASE_URL não definida.');
 }
 
-const adapter = new PrismaNeon({ connectionString: DATABASE_URL });
+const adapter = new PrismaNeon({
+  connectionString: DATABASE_URL
+});
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient;
+};
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ adapter, log: ['error'] });
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log: ['error']
+  });
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
